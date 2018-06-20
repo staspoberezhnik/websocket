feb037f662b5d93b753b2e36d9a4be416d1b08b5
Vladislav Zaynchkovsky <zvsx001@gmail.com>
2018-06-20 17:50:29 +0300

    add PrivateHandler private.html

--- chat.py
+++ chat.py
@@ -3,7 +3,6 @@ import tornado.web
 import tornado.websocket
 import asyncpg
 from tornado import escape
-import asyncio
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -19,7 +18,6 @@ class MainHandler(BaseHandler):
             self.redirect("/login")
         else:
             users = []
-
             name = tornado.escape.xhtml_escape(self.get_current_user())
             users.append(name)
             self.render("base.html", users=users)
@@ -50,28 +48,51 @@ class SimpleWebSocket(BaseHandler, tornado.websocket.WebSocketHandler):
         return True
 
     def open(self):
-
         self.connections.add(self)
 
     async def on_message(self, message):
 
         [client.write_message(message) for client in self.connections]
 
+        data = tornado.escape.json_decode(message)
+
+        db_connection = await asyncpg.connect(user='provider',
+                                              password='12345',
+                                              database='websocket',
+                                              host='127.0.0.1',
+                                              port='5432'
+                                              )
+        await db_connection.execute('''
+                INSERT INTO chat(author, message) VALUES($1, $2)
+            ''', data['user'], data['message'])
+
+        # values = await db_connetion.fetch('''SELECT * FROM chat''')
+        # for value in values:
+        #     print(value)
+
+        await db_connection.close()
+
     def on_close(self):
         self.connections.remove(self)
 
 
-class PrivateMessage(SimpleWebSocket):
-    pass
+class PrivateHandler(tornado.web.RequestHandler):
+
+    def get(self, user):
+        users = []
+        print(user)
+        name = self.get_secure_cookie('user')
+        users.append(name)
+        self.render("private.html", users=users)
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/login", LoginHandler),
-        (r'/logout',LogoutHandler),
+        (r'/logout', LogoutHandler),
         (r"/websocket", SimpleWebSocket),
-        (r'/privatmes', PrivateMessage),
+        (r"/privatmessage/(?P<user>[-\w]+)/$", PrivateHandler),
     ],
         cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__")
 
